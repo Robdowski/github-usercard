fa88d6de9c97b412e6fa45b23802a8bbebba0552
Robert <rc3331@gmail.com>
2019-09-05 13:45:29 -0500

    completed MVP and stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,8 +24,6 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -43,8 +41,8 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
+
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -53,3 +51,84 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+////// CARD CONSTRUCTOR //////
+
+function gitCard(request) {
+  // TOP LEVEL
+  const card = document.createElement("div");
+  card.classList.add("card");
+  // SECOND LEVEL
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+  // THIRD LEVEL
+  const h3 = document.createElement("h3");
+  h3.classList.add("name");
+  const username = document.createElement("p");
+  username.classList.add("username");
+  const location = document.createElement("p");
+  const profile = document.createElement("p"); /// APPEND AN ANCHOR TO THIS
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+  // FOURTH LEVEL
+  const anchor = document.createElement("a"); /// APPEND TO PROFILE
+
+  ///// STRUCTURE
+  // level 1
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  // level 2
+  cardInfo.appendChild(h3);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  //level 3
+  profile.appendChild(anchor);
+
+  //// ADDING DATA
+  image.src = request.avatar_url;
+  h3.textContent = request.name;
+  username.textContent = request.login;
+  location.textContent = `Location: ${request.location}`;
+  followers.textContent = `Followers: ${request.followers}`;
+  following.textContent = `Following: ${request.following}`;
+  bio.textContent = `Bio: ${request.bio}`;
+
+  anchor.setAttribute("href", request.html_url);
+  anchor.textContent = request.html_url;
+
+  return card;
+}
+
+/////// MY CARD //////
+
+const container = document.querySelector(".cards");
+axios.get("https://api.github.com/users/Robdowski").then(res => {
+  //console.log(res)
+  const newCard = container.appendChild(gitCard(res.data));
+});
+
+/////// FOLLOWERS CARDS ///////
+
+const followersArray = []
+
+
+/// ADD FOLLOWERS TO ARRAY AND THEN ADD THEM AS CARDS
+axios
+.get("https://api.github.com/users/Robdowski/followers")
+.then(res => {
+  console.log(res);
+  res.data.forEach(item => followersArray.push(item.url));
+})
+.then(res =>{
+  followersArray.forEach(item => {
+    axios.get(item).then(res => {
+      container.appendChild(gitCard(res.data))
+    })
+  })
+})
